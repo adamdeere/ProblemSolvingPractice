cdf9e2c0c0c71d46757199fd610b31c1cad1a371
Adam <adam.deere@hotmail.com>
2023-05-18 18:36:05 +0100

    Update Program.cs
    The solution was correct

--- ProblemSolvingPractice/ProblemSolvingPractice/Program.cs
+++ ProblemSolvingPractice/ProblemSolvingPractice/Program.cs
@@ -4,7 +4,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        int range = 4;
+        int range = 13;
         ICalculateProduct m = new LargestNumberInSeries();
         long result = m.CalculateResults(range);
         Console.WriteLine("The result is : " + result);
